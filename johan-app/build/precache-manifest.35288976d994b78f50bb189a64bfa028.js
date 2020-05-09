79538ae6a2ade1de0681fd8c23d6ef8cea3c2e3f
ckalonzo <ckalonzo@gmail.com>
2020-05-09 17:23:25 -0400

    update

---
+++ johan-app/build/precache-manifest.35288976d994b78f50bb189a64bfa028.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e45a97f2d91ed03b14938a98d1e8009",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e9d0aaa3b44b5bbe92e",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "c73b2878d1336dc05dd8",
+    "url": "/static/css/main.9de143a5.chunk.css"
+  },
+  {
+    "revision": "6e9d0aaa3b44b5bbe92e",
+    "url": "/static/js/2.c2cb2253.chunk.js"
+  },
+  {
+    "revision": "f7eb6ac34a33a7724223c95fd0206cb9",
+    "url": "/static/js/2.c2cb2253.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c73b2878d1336dc05dd8",
+    "url": "/static/js/main.5135b41e.chunk.js"
+  },
+  {
+    "revision": "c793ebf059d29bef793f",
+    "url": "/static/js/runtime-main.b0aa2be8.js"
+  }
+]);
