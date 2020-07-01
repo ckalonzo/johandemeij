a5e387214c466596c6e0a8b5c6fbc2c68db81b0d
ckalonzo <ckalonzo@gmail.com>
2020-07-01 15:47:23 -0400

    updated biography header image

---
+++ johan-app/build/precache-manifest.f913e92691c988d30f449e934575bfdb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5efe5becac82f22c96f3e19a0662a47c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ecf989e3f694e486180f",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "ee4df4501e3de42ea157",
+    "url": "/static/css/main.a6a94418.chunk.css"
+  },
+  {
+    "revision": "ecf989e3f694e486180f",
+    "url": "/static/js/2.ed6a9213.chunk.js"
+  },
+  {
+    "revision": "03a9249160ac3bebe7b52a7ae0fb20b3",
+    "url": "/static/js/2.ed6a9213.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee4df4501e3de42ea157",
+    "url": "/static/js/main.83e3032d.chunk.js"
+  },
+  {
+    "revision": "c793ebf059d29bef793f",
+    "url": "/static/js/runtime-main.b0aa2be8.js"
+  }
+]);
