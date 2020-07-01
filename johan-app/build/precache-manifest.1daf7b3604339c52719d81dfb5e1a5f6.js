85e8410c9eda7564597501fa674b16160b2d9b9d
carl kalonzo <laurakalonzo@Carls-MacBook.local>
2020-07-01 15:49:46 -0400

    old files

---
+++ johan-app/build/precache-manifest.1daf7b3604339c52719d81dfb5e1a5f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbd981c912cef77bc0e2eb8f93f2289e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98b289ded8c62ec6d66d",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "7bfe7ef7cc5a4570f149",
+    "url": "/static/css/main.a6a94418.chunk.css"
+  },
+  {
+    "revision": "98b289ded8c62ec6d66d",
+    "url": "/static/js/2.668ca862.chunk.js"
+  },
+  {
+    "revision": "03a9249160ac3bebe7b52a7ae0fb20b3",
+    "url": "/static/js/2.668ca862.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bfe7ef7cc5a4570f149",
+    "url": "/static/js/main.0ce709da.chunk.js"
+  },
+  {
+    "revision": "c793ebf059d29bef793f",
+    "url": "/static/js/runtime-main.b0aa2be8.js"
+  }
+]);
