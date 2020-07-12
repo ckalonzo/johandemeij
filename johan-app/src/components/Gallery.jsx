051a4b76a459f833573b1c9223ff49e2a4de0f3a
carl kalonzo <laurakalonzo@Carls-MacBook.local>
2020-07-12 12:53:27 -0400

    updates

--- johan-app/src/components/Gallery.jsx
+++ johan-app/src/components/Gallery.jsx
@@ -1,13 +1,13 @@
 import React, { useState, useEffect, useCallback } from "react";
 import { connect } from "react-redux";
-import Carousel, { Modal, ModalGateway } from "react-images";
 import { Button } from "react-bootstrap";
 import ReactHtmlParser from "react-html-parser";
 import { bindActionCreators } from "redux";
 import { ACTIONS } from "redux/actions/types";
 import { mainAction } from "redux/actions/index.actions";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
 import dayjs from "dayjs";
-import AliceCarousel from 'react-alice-carousel'
 import 'react-alice-carousel/lib/alice-carousel.css'
 const PhotoGallery = (props) => {
   const [ currentIndex, setCurrentIndex ] = useState(0)
@@ -35,16 +35,8 @@ const PhotoGallery = (props) => {
       </ul>)}
       
   const renderGallery =(data)=> {
-    return (<AliceCarousel mouseTrackingEnabled
-      dotsDisabled={true}
-      buttonsDisabled={true}
-      slideToIndex={currentIndex}
-      onSlideChanged={onSlideChanged}
-    >
-      { [1,2,3,4,5].map((photo, i) =>{ 
-        console.log(photo)
-        return(<div key={i} className="yours-custom-class"><h2>{ photo.name }</h2></div>)}) }
-    </AliceCarousel>);
+    return (<>
+    </>);
   }
   const handleOnDragStart = (e) => e.preventDefault()
   return (<>
@@ -52,11 +44,22 @@ const PhotoGallery = (props) => {
     <div className="container">
       <div className="row">
       <div className="col-10 offset-1 text-center">
-      { renderThumbs() }
-          <Button variant="secondary" onClick={() => slidePrev()}>Prev button</Button>{" "}
-          <Button variant="dark" onClick={() => slideNext()}>Next button</Button>
+     
         <h1>Gallery</h1>
-      {renderGallery(props.photos)}
+        <Carousel autoPlay interval="5000" transitionTime="1000">
+                    <div>
+                        <img src={`https://firebasestorage.googleapis.com/v0/b/johandemeij-513b2.appspot.com/o/gallery%2Fimage1.jpg?alt=media`} width="100%" height="100%" object-fit="cover"/>
+                        <p className="legend">My Classic Still 1</p>
+                    </div>
+                    <div>
+                        <img src={`https://firebasestorage.googleapis.com/v0/b/johandemeij-513b2.appspot.com/o/gallery%2Fimage2.jpg?alt=media`} width="100%" height="100%" object-fit="cover"/>
+                        <p className="legend">My Classic Still 2</p>
+                    </div>
+                    <div>
+                        <img src={`https://firebasestorage.googleapis.com/v0/b/johandemeij-513b2.appspot.com/o/gallery%2Fimage3.jpg?alt=media`}  width="100%" height="100%" object-fit="cover"/>
+                        <p className="legend">My Classic Still 3</p>
+                    </div>
+                </Carousel>
       </div>
       </div>
     </div>
