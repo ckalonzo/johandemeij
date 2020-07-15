f1e18afc10769848cbb290dedf6177587e6a42aa
ckalonzo <ckalonzo@gmail.com>
2020-07-15 15:43:16 -0400

    completed gallery

--- johan-app/src/components/Gallery.jsx
+++ johan-app/src/components/Gallery.jsx
@@ -1,21 +1,19 @@
 import React, { useState, useEffect, useCallback } from "react";
 import { connect } from "react-redux";
-import { Button } from "react-bootstrap";
-import ReactHtmlParser from "react-html-parser";
 import { bindActionCreators } from "redux";
 import { ACTIONS } from "redux/actions/types";
 import { mainAction } from "redux/actions/index.actions";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from 'react-responsive-carousel';
-import dayjs from "dayjs";
+import LazyLoad from 'react-lazyload';
 import 'react-alice-carousel/lib/alice-carousel.css'
+
 const PhotoGallery = (props) => {
   const [ currentIndex, setCurrentIndex ] = useState(0)
-  const [ items, setItems ] = useState([1,2,3,4,5])
   const slideTo = (i) => setCurrentIndex({ currentIndex: i });
   const slideNext = () => setCurrentIndex({ currentIndex: currentIndex + 1});
   const slidePrev = () => setCurrentIndex({ currentIndex: currentIndex - 1 });
-  const [onSlideChanged,setSlideChanged] = useState() 
+  const [fadeOut,setFadeOut] = useState()
+  const [slideLocation,setSlideLocation] = useState(0) 
   useEffect(() => {
     document.title = "JohanDeMeij.com | Home";
     props.actions.mainAction(ACTIONS.LOAD_GALLERY,[])
@@ -24,15 +22,27 @@ const PhotoGallery = (props) => {
 
   const renderImages = () => {
    return Object.values(props.photos).map(photo=>{
-      return ( <div><img src={`https://firebasestorage.googleapis.com/v0/b/johandemeij-513b2.appspot.com/o/gallery%2F${photo.name}?alt=media`} width="100%" height="100%" object-fit="cover"/>
-      {photo.caption ? <p className="legend">{photo.caption}</p>:""}</div>)
+      return ( <li className={"slide "+fadeOut}><LazyLoad width="100%" height="100%" object-fit="contain" ><img src={`https://firebasestorage.googleapis.com/v0/b/johandemeij-513b2.appspot.com/o/gallery%2F${photo.name}?alt=media`} width="100%" height="100%" object-fit="contain"/>
+      {photo.caption ? <p className="legend">{photo.caption}</p>:""}</LazyLoad></li>)
+    })
+  }
+const slideImage = (item) =>{
+  window.scrollTo(0,0)
+  setFadeOut("fade-in")
+  setTimeout(()=>{
+    setFadeOut("")
+  },1000)
+  let location = (item * 100)
+  return setSlideLocation(`-${location}%`)
+}
+  const renderThumbnails = () => {
+    return Object.values(props.photos).map((photo,i)=>{
+      return ( <li onClick={()=>slideImage(i)} className="thumb" ariaLabel={"slide item "+i} role="button" tabindex="0" style={{width: "80px"}}><LazyLoad width="100%" height="100%" object-fit="cover" ><img src={`https://firebasestorage.googleapis.com/v0/b/johandemeij-513b2.appspot.com/o/gallery%2F${photo.name}?alt=media`} width="100%" height="100%" object-fit="cover"/></LazyLoad>
+      </li>)
     })
   }
 
-  const renderGallery =(data)=> {
-    return (<>
-    </>);
-  }
+
   const handleOnDragStart = (e) => e.preventDefault()
   return (<>
   <section className="photo-gallery">
@@ -40,7 +50,13 @@ const PhotoGallery = (props) => {
       <div className="row">
       <div className="col-10 offset-1 text-center">
         <h1 style={{color: "rgb(0, 0, 0)", textAlign:"center", marginBottom: "25px"}}>Gallery</h1>
-  <Carousel autoPlay interval="5000" transitionTime="1000">{renderImages()}</Carousel>
+    <div className="galleryContainer">
+    <div width="100%" height="100%" object-fit="cover" className="vail"></div>
+      <ul style={{transform: `translate3d(${slideLocation}, 0px, 0px)`, transitionDuration:" 650ms"}}>{renderImages()}</ul>
+      
+    </div>
+    
+    <ul>{renderThumbnails()}</ul>
       </div>
       </div>
     </div>
