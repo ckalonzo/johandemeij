8372a2ffa9c37d9ca7411acb57ec1f2b1574dcd2
carl kalonzo <laurakalonzo@Carls-MacBook.local>
2020-07-19 19:42:49 -0400

    updating reactga code

---
+++ johan-app/build/precache-manifest.5dbfe66ba0015c95b0a9125f442b507c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03671ea355fc9e1e85fa94444a23ef79",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04229fdfbea0aeb36124",
+    "url": "/static/css/2.c89b348a.chunk.css"
+  },
+  {
+    "revision": "650b155d542ed05c8b0e",
+    "url": "/static/css/main.54e5ab3c.chunk.css"
+  },
+  {
+    "revision": "04229fdfbea0aeb36124",
+    "url": "/static/js/2.8127f7c9.chunk.js"
+  },
+  {
+    "revision": "03a9249160ac3bebe7b52a7ae0fb20b3",
+    "url": "/static/js/2.8127f7c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "650b155d542ed05c8b0e",
+    "url": "/static/js/main.348bc1c0.chunk.js"
+  },
+  {
+    "revision": "c793ebf059d29bef793f",
+    "url": "/static/js/runtime-main.b0aa2be8.js"
+  }
+]);
--- johan-app/build/service-worker.js
+++ johan-app/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.393ddca5897551f2c05ef023157082e0.js"
+  "/precache-manifest.5dbfe66ba0015c95b0a9125f442b507c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
